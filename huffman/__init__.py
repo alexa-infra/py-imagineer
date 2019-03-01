fdbf2592ff74301f228d06808d830388725d8961
Alexey Vasilyev <alexa.infra@gmail.com>
2019-03-01 12:50:24 +0100

    encode/decode huffman table imports

--- huffman/__init__.py
+++ huffman/__init__.py
@@ -1,4 +1,4 @@
 from .core import get_frequences
 from .core import get_huffman_table
-from .encoding import encode
-from .decoding import decode
+from .encoding import encode, encode_table
+from .decoding import decode, decode_table
